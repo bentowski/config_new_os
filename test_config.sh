bbd261f904b77102b646525dba5cc94abe01e782
bentowski <benjamin.baudry@hotmail.com>
2022-07-09 14:57:12 +0200

    add make clang valgrind

--- test_config.sh
+++ test_config.sh
@@ -6,5 +6,6 @@ touch ~/$af
 #sudo apt install source -y
 echo "alias sap='sudo apt -y'" > ~/$af
 echo "alias upg='sudo apt update && sudo apt -y upgrade'" >> ~/$af
+sudo apt install make clang valgrind -y
 
 #source ~/.bash_aliases
